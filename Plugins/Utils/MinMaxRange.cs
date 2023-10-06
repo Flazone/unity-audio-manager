7e74ee1ee498444eace335aca22aa45cff389c25
Flavien Caston <flaviencaston@gmail.com>
2023-10-06 16:26:20 +0100

    Optimized MinMaxRange generating a ton of garbage when asking for a random value, now using Unity built-in random

--- Plugins/Utils/MinMaxRange.cs
+++ Plugins/Utils/MinMaxRange.cs
@@ -1,4 +1,6 @@
 using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public abstract class MinMaxRange<T>
 {
@@ -10,7 +12,7 @@ public abstract class MinMaxRange<T>
     /// Returns a random value between [rangeStart, rangeEnd] using the provided random function.
     /// </summary>
     /// <param name="MinMaxInclusiveRandomFunction">Inclusive random function that takes a minimum and a maximum value as parameters.</param>
-    public T GetRandomValue(System.Func<T, T, T> MinMaxInclusiveRandomFunction)
+    protected T GetRandomValue(System.Func<T, T, T> MinMaxInclusiveRandomFunction)
     {
         return MinMaxInclusiveRandomFunction(RangeMin, RangeMax);
     }    
@@ -23,33 +25,12 @@ public abstract class MinMaxRange<T>
 [Serializable]
 public class MinMaxRange : MinMaxRange<float>
 {
-    public override float Value => GetRandomValue((min, max) => Lerp(min, max, (float)new Random().NextDouble()));
+    public override float Value => GetRandomValue((min, max) => Mathf.Lerp(min, max, Random.value));
 
     public override float RangeMiddle => RangeMin + ((RangeMax - RangeMin) * 0.5f);
 
     public override bool IsInRange(float value)
     {
         return !((value > RangeMax) || (value < RangeMin));
-    }
-
-    private float Lerp(float a, float b, float t)
-    {
-        t = Clamp(t, 0f, 1f);
-        return (a * (1f - t) + (b * t));
-    }
-
-    private T Clamp<T>(T x, T min, T max) where T : System.IComparable
-    {
-        var _Result = x;
-        if (x.CompareTo(max) > 0)
-        {
-            _Result = max;
-        }
-        else if (x.CompareTo(min) < 0)
-        {
-            _Result = min;
-        }
-        
-        return _Result;
     }
 }
